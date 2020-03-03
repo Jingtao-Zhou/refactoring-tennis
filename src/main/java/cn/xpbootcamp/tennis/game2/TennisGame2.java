1f420129fc6d7ecd5077477b4f5d13f64be4edcc
Jingtao Zhou <jingtao.zhou@thoughtworks.com>
2020-03-03 23:12:41 +0800

    replace some logic with map

--- src/main/java/cn/xpbootcamp/tennis/game2/TennisGame2.java
+++ src/main/java/cn/xpbootcamp/tennis/game2/TennisGame2.java
@@ -1,5 +1,8 @@
 package cn.xpbootcamp.tennis.game2;
 
+import cn.xpbootcamp.tennis.DeuceScoreMap;
+import cn.xpbootcamp.tennis.NotEqualHighScoreMap;
+import cn.xpbootcamp.tennis.NotEqualLowScoreMap;
 import cn.xpbootcamp.tennis.TennisGame;
 
 public class TennisGame2 implements TennisGame {
@@ -14,77 +17,39 @@ public class TennisGame2 implements TennisGame {
 
     public String getScore() {
         String score = "";
-        if (player1Score == player2Score && player1Score < 4) {
-            if (player1Score == 0)
-                score = "Love";
-            if (player1Score == 1)
-                score = "Fifteen";
-            if (player1Score == 2)
-                score = "Thirty";
-            score += "-All";
+        if (player1Score == player2Score) {
+            score = DeuceScoreMap.getResult(player1Score);
         }
-        if (player1Score == player2Score && player1Score >= 3)
-            score = "Deuce";
 
-        if (player1Score > 0 && player2Score == 0) {
-            if (player1Score == 1)
-                player1Result = "Fifteen";
-            if (player1Score == 2)
-                player1Result = "Thirty";
-            if (player1Score == 3)
-                player1Result = "Forty";
-
+        if (player1Score > 0 && player1Score < 4 && player2Score == 0) {
+            player1Result = NotEqualLowScoreMap.getResult(player1Score);
             player2Result = "Love";
             score = player1Result + "-" + player2Result;
         }
-        if (player2Score > 0 && player1Score == 0) {
-            if (player2Score == 1)
-                player2Result = "Fifteen";
-            if (player2Score == 2)
-                player2Result = "Thirty";
-            if (player2Score == 3)
-                player2Result = "Forty";
+        if (player2Score > 0 && player2Score < 4 && player1Score == 0) {
+            player2Result = NotEqualLowScoreMap.getResult(player2Score);
 
             player1Result = "Love";
             score = player1Result + "-" + player2Result;
         }
 
         if (player1Score > player2Score && player1Score < 4) {
-            if (player1Score == 2)
-                player1Result = "Thirty";
-            if (player1Score == 3)
-                player1Result = "Forty";
-            if (player2Score == 1)
-                player2Result = "Fifteen";
-            if (player2Score == 2)
-                player2Result = "Thirty";
+            player1Result = NotEqualLowScoreMap.getResult(player1Score);
+
+            player2Result = NotEqualLowScoreMap.getResult(player2Score);
+
             score = player1Result + "-" + player2Result;
         }
         if (player2Score > player1Score && player2Score < 4) {
-            if (player2Score == 2)
-                player2Result = "Thirty";
-            if (player2Score == 3)
-                player2Result = "Forty";
-            if (player1Score == 1)
-                player1Result = "Fifteen";
-            if (player1Score == 2)
-                player1Result = "Thirty";
+            player1Result = NotEqualLowScoreMap.getResult(player1Score);
+
+            player2Result = NotEqualLowScoreMap.getResult(player2Score);
+
             score = player1Result + "-" + player2Result;
         }
 
-        if (player1Score > player2Score && player2Score >= 3) {
-            score = "Advantage player1";
-        }
-
-        if (player2Score > player1Score && player1Score >= 3) {
-            score = "Advantage player2";
-        }
-
-        if (player1Score >= 4 && player2Score >= 0 && (player1Score - player2Score) >= 2) {
-            score = "Win for player1";
-        }
-        if (player2Score >= 4 && player1Score >= 0 && (player2Score - player1Score) >= 2) {
-            score = "Win for player2";
+        if ((player1Score > 3 || player2Score > 3) && player1Score != player2Score) {
+            score = NotEqualHighScoreMap.getResult(player1Score - player2Score);
         }
         return score;
     }
